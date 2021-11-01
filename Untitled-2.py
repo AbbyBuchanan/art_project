510b14257989745d9915fd6fba6e0ab1a2232d59
AbbyBuchanan <91632408+AbbyBuchanan@users.noreply.github.com>
2021-11-01 11:15:44 -0700

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -6,13 +6,22 @@ orange_circle = "orange"
 shape = "circle"
 size = 5
 
+
 circle.shape(shape)
 circle.fillcolor(orange_circle)
 circle.shapesize(size)
 
 circle.penup()
+count = 0
 def circle_clicked(x, y):
+  global count
   change_position()
+  count += 1
+  print(count)
+  suprise = rand.randint(1,20)
+  if count == suprise:
+    circle.shape('confetti-10.gif')
+
 
 def change_position():
   new_xpos = rand.randint(-300,300)
@@ -21,13 +30,9 @@ def change_position():
 
 
 
-
-
-
-
-
 wn = trtl.Screen()
 circle.onclick(circle_clicked)
 wn.setup(width=800,height=800)
-wn.addshape('confetti.gif')
+wn.bgpic("fire.gif")
+wn.addshape('confetti-10.gif')
 wn.mainloop()
